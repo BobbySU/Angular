bc72ed6dc67f1766e8d985011836c283a0695aaa
BobbySU <radio_20bg@yahoo.com>
2022-11-23 18:48:16 +0200

    DI, Intro to RxJS, Services - setInterval

--- DI, Intro to RxJS, Services/second-project/src/app/app.component.ts
+++ DI, Intro to RxJS, Services/second-project/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'second-project';
+
+  count = 0;
+  constructor() {
+    setInterval(() => {
+      this.count++;
+    }, 3000);
+  }
 }
