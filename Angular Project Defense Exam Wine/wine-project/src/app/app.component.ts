1788ca5e9714d0e7a3c72c1ef021a462f508f817
BobbySU <radio_20bg@yahoo,com>
2024-04-12 23:33:48 +0300

    Add Main

--- Angular Project Defense Exam Wine/wine-project/src/app/app.component.ts
+++ Angular Project Defense Exam Wine/wine-project/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CoreModule } from './core/core.module';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
     CoreModule,
-
+    MainComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
