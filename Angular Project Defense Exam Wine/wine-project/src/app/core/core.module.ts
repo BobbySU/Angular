23e346700745c566071e46e28cc64f00d16002e9
BobbySU <radio_20bg@yahoo,com>
2024-04-12 22:37:50 +0300

    Add Header and Footer page

--- Angular Project Defense Exam Wine/wine-project/src/app/core/core.module.ts
+++ Angular Project Defense Exam Wine/wine-project/src/app/core/core.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
+import { HeadarComponent } from './headar/headar.component';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    FooterComponent,
+    HeadarComponent
+  ],
+  exports: [
+    HeadarComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
