4cbe0ad7224527247e51a2d150e76f1257c97da6
BobbySU <radio_20bg@yahoo,com>
2024-04-12 22:30:11 +0300

    Create headar and footer

---
+++ Angular Project Defense Exam Wine/wine-project/src/app/core/headar/headar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headar',
+  standalone: true,
+  imports: [],
+  templateUrl: './headar.component.html',
+  styleUrl: './headar.component.scss'
+})
+export class HeadarComponent {
+
+}
