e864a659a6a9070be3f9cc9df7cce5e4befa5c88
BobbySU <radio_20bg@yahoo,com>
2024-04-14 03:43:34 +0300

    Add LogIn Validator

--- Angular Project Defense Exam Wine/wine-project/src/app/auth/login/login.component.ts
+++ Angular Project Defense Exam Wine/wine-project/src/app/auth/login/login.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    FormsModule,
+    SharedModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  appEmailDomains = ['bg', 'com'];
 
 }
