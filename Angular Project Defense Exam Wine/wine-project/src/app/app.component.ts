6156f598c21049f2b2f48275841026b8d37a1844
BobbySU <radio_20bg@yahoo,com>
2024-11-30 21:10:30 +0200

    del MainComponent

--- Angular Project Defense Exam Wine/wine-project/src/app/app.component.ts
+++ Angular Project Defense Exam Wine/wine-project/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CoreModule } from './core/core.module';
-import { MainComponent } from './main/main.component';
 
 @Component({
     selector: 'app-root',
     imports: [RouterOutlet,
-        CoreModule,
-        MainComponent
+        CoreModule
     ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
